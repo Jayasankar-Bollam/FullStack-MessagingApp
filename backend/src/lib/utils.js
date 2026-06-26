a73d69a617198a3b3f3f26c086d8a496646911a1
Jayasankar-Bollam <jayasankarbollam01@gmail.com>
2026-06-26 08:53:46 +0530

    fix cookie for cross domain production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,17 +14,9 @@ import jwt from "jsonwebtoken";
 
 //   return token;
 // };
-export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: "none", // for cross-site cookie
-    secure: true,     // required when sameSite is 'none'
-  });
-
-  return token;
-};
+res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  secure: process.env.NODE_ENV === "production",
+});

600826980b40b663f1c63e228a7403b9c782131f
Jayasankar-Bollam <jayasankarbollam01@gmail.com>
2026-06-26 10:29:22 +0530

    some fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : import.meta.env.VITE_BACKEND_URL + "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : import.meta.env.VITE_BACKEND_URL + "/api",
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
